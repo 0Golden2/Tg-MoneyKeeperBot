3796754436a2f64e629836a552b2773c343f0f25
DimaSv <111643005+0Golden2@users.noreply.github.com>
2022-10-20 01:33:19 +0300

    Добавил приветственное сообщение

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-from aiogram import Bot
-print('Делай бота быстрее')
+from aiogram import Bot, Dispatcher, types
+import os
 
+
+API_TOKEN = os.getenv('5466461965:AAGlL5aTei7QVRiQ6d55ys1rwx74gfY-pHE')
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def welcome_message(message: types.Message):
+    """Отправляет приветственное сообщение"""
+    await message.answer(
+        "Привет, я бот для учета финансов\n\n"
+        "Добавить расход: сумма_категория (Пример: 100 магазин)\n"
+        "Месячный план трат: _\n"
+        "Траты за сегодня: /today\n"
+        "Траты за текущий месяц: /month\n"
+        "Последние внесенные расходы: /lexpenses\n"
+        "Категории трат: /categories\n"
+    )
