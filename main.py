e98d41365b958a2e5d456c49fc986486de486705
DimaSv <111643005+0Golden2@users.noreply.github.com>
2022-11-01 20:20:29 +0300

    добавил файл midlewares.py для того, чтобы бот получал сообщения только от меня

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, executor
 import os
+import logging
+from middlewares import AccessMiddleware
 
+logging.basicConfig(level=logging.INFO)
 
-API_TOKEN = os.getenv('5466461965:AAGlL5aTei7QVRiQ6d55ys1rwx74gfY-pHE')
+API_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
+ACCESS_ID = int(os.getenv('TELEGRAM_ACCESS_ID'))
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+dp.middleware.setup(AccessMiddleware(ACCESS_ID))
 
 
-@dp.message_handler(commands=['start', 'help'])
-async def welcome_message(message: types.Message):
-    """Отправляет приветственное сообщение"""
-    await message.answer(
-        "Привет, я бот для учета финансов\n\n"
-        "Добавить расход: сумма_категория (Пример: 100 магазин)\n"
-        "Месячный план трат: _\n"
-        "Траты за сегодня: /today\n"
-        "Траты за текущий месяц: /month\n"
-        "Последние внесенные расходы: /lexpenses\n"
-        "Категории трат: /categories\n"
-    )
+HELP_MESSAGE = """Добавить расход: сумма_категория (Пример: 100 магазин)\n
+                Месячный план трат: _\n
+                Траты за сегодня: /today\n
+                Траты за текущий месяц: /month\n
+                Последние внесенные расходы: /lexpenses\n
+                Категории трат: /categories\n"""
+
+
+@dp.message_handler(commands=['start'])
+async def welcome_message(message: types.Message):                  #Отправляет приветственное сообщение
+    await message.answer(text="Привет, я бот для учета финансов")
+    await message.delete()
+
+@dp.message_handler(commands=['help'])
+async  def help_message(message: types.Message):        #Отправляет сообщение с функциями бота
+    await message.reply(text=HELP_MESSAGE)
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
