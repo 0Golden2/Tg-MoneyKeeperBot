defd758c42235acfaa11fb87e78c05dfdb1e650c
DimaSv <111643005+0Golden2@users.noreply.github.com>
2022-11-01 21:18:44 +0300

    удалил midlewares.py, тк написал функцию авторизации в main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from aiogram import Bot, Dispatcher, types, executor
 import os
 import logging
-from middlewares import AccessMiddleware
+from aiogram.dispatcher.handler import CancelHandler
+
 
 logging.basicConfig(level=logging.INFO)
 
@@ -9,7 +10,6 @@ API_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
 ACCESS_ID = int(os.getenv('TELEGRAM_ACCESS_ID'))
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
-dp.middleware.setup(AccessMiddleware(ACCESS_ID))
 
 
 HELP_MESSAGE = """Добавить расход: сумма_категория (Пример: 100 магазин)\n
@@ -20,15 +20,27 @@ HELP_MESSAGE = """Добавить расход: сумма_категория (
                 Категории трат: /categories\n"""
 
 
+def auth(func):
+    async def wrapper(message):
+        if int(message.from_user.id) != ACCESS_ID:
+            await message.answer(text="Access denied")
+            raise CancelHandler()
+        return await func(message)
+    return wrapper
+
+
 @dp.message_handler(commands=['start'])
+@auth
 async def welcome_message(message: types.Message):                  #Отправляет приветственное сообщение
     await message.answer(text="Привет, я бот для учета финансов")
     await message.delete()
 
+
 @dp.message_handler(commands=['help'])
-async  def help_message(message: types.Message):        #Отправляет сообщение с функциями бота
+@auth
+async def help_message(message: types.Message):        #Отправляет сообщение с функциями бота
     await message.reply(text=HELP_MESSAGE)
 
 
 if __name__ == "__main__":
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
